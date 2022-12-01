7aec7abd2ce204a3a837757b87f26e79b63a1210
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2022-12-01 19:56:05 +0530

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>WEB to PDF</title>
+        <meta name="description" content="Convert webpages to PDFs" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Welcome!</h1>
+    </div>
+  );
+}
---
+++ setup.md
@@ -0,0 +1,2 @@
+Create NextJs Project  
+`npx create-next-app@12.2.0 web-to-pdf --use-npm`
